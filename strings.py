cdc19a5579e151dca5d9b9a99dbf3832843d4d4e
shalom <shlomo000@gmail.com>
2024-10-21 23:15:57 +0300

    initilze commit - class structure

---
+++ strings.py
@@ -0,0 +1,24 @@
+a = 1     # int
+b = 'x'   # str
+c = False # bool
+d = []    # list
+e = {}    # dict
+f = ()    # tuple
+g = set() # set    =>  מכיל רק ערכים ייחודיים
+
+###################################
+
+my_list = [1, 2, 3, 4]
+
+person = {
+    'name': 'chaim',
+    'age': 25,
+    'address': {
+        'city': 'emanuel',
+        'street': 'rabi meir',
+        'hom number': 34
+    }
+}
+
+for i in range(len(my_list)):
+    print(my_list[i])
